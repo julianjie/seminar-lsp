42d67dd3e5ef7c25da5c54211697239aa99f4e18
Julian Jie <julian.jie78@gmail.com>
2026-07-14 13:39:27 +0700

    Fix admin accepting user

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,44 +2,31 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'no_hp',
+        'alamat',
+        'status_akun',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
@@ -47,6 +34,7 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
     public function seminarRegistrations(): HasMany
     {
         return $this->hasMany(SeminarRegistration::class);
